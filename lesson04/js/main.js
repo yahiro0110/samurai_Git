97bb0173e8ee9f82431cbfb81ee4fb490f12c0e4
yahiro0110 <yama01hiro@gmail.com>
2020-09-21 08:14:42 +0900

    2cd Commit:Fix

---
+++ lesson04/js/main.js
@@ -0,0 +1,40 @@
+/*--------------------------------------------------- 
+関数の定義
+---------------------------------------------------*/
+// tdタグの生成
+function create_td(params) {
+  let newtd = document.createElement('td');
+  newtd.innerHTML = params;
+  newtr.appendChild(newtd);
+}
+// 3の数字がつくもの、もしくは３の倍数の場合は以下のtdタグを使用
+function create_td3(params) {
+  let newtd = document.createElement('td');
+  newtd.classList.add('td__3')
+  newtd.innerHTML = params;
+  newtr.appendChild(newtd);
+}
+
+/*--------------------------------------------------- 
+テーブルの生成
+---------------------------------------------------*/
+// table要素の取得
+var table = document.getElementById('table_id');
+// テーブルの作成
+for (let num = 0; num < 10; num++) {
+  var newtr = table.insertRow(table.rows.length);
+  for (let i = 0; i < 10; i++) {
+    let checkNum = num * 10 + i;
+    // checkNumが0ではない、かつ３の倍数、もしくは３がつく場合はTrueを返す
+    switch ((checkNum !== 0 && checkNum % 3 == 0) || String(checkNum).includes('3')) {
+      case true:
+        create_td3(checkNum);
+        break;
+      case false:
+        create_td(checkNum);
+        break;
+      default:
+        break;
+    }
+  }
+}
