25c4b9a34b4ff06c2a1b7d85a8591164f7f07038
yahiro0110 <yama01hiro@gmail.com>
2020-09-28 05:04:19 +0900

    3rd Commit：var newtrをlet newtrに変更

--- lesson04/js/main.js
+++ lesson04/js/main.js
@@ -2,13 +2,13 @@
 関数の定義
 ---------------------------------------------------*/
 // tdタグの生成
-function create_td(params) {
+function create_td(newtr, params) {
   let newtd = document.createElement('td');
   newtd.innerHTML = params;
   newtr.appendChild(newtd);
 }
 // ３の倍数、もしくは３の数字がつくものは以下のtdタグを使用
-function create_td3(params) {
+function create_td3(newtr, params) {
   let newtd = document.createElement('td');
   newtd.classList.add('td__3')
   newtd.innerHTML = params;
@@ -22,16 +22,16 @@ function create_td3(params) {
 var table = document.getElementById('table_id');
 // テーブルの作成
 for (let num = 0; num < 10; num++) {
-  var newtr = table.insertRow(table.rows.length);
+  let newtr = table.insertRow(table.rows.length);
   for (let i = 0; i < 10; i++) {
     let checkNum = num * 10 + i;
     // checkNumが0ではない、かつ３の倍数、もしくは３がつく場合はTrueを返す
     switch ((checkNum !== 0 && checkNum % 3 == 0) || String(checkNum).includes('3')) {
       case true:
-        create_td3(checkNum);
+        create_td3(newtr, checkNum);
         break;
       case false:
-        create_td(checkNum);
+        create_td(newtr, checkNum);
         break;
       default:
         alert("Error:switch部分で正常に処理できませんでした");
