6c33a9aaf65bdf46eb08788ae766435b08a6b51f
yahiro0110 <yama01hiro@gmail.com>
2020-09-30 07:13:36 +0900

    3rd Commit：プロト作成

--- lesson05/js/main.js
+++ lesson05/js/main.js
@@ -1,11 +1,27 @@
 const stalker = document.getElementById('mouse_stalker');
 
-function getMouseXY (e) {
-  let mX = e.pageX;
-  let mY = e.pageY;
-  // document.getElementById('mouse_stalker').innerText = "X座標：" + mX + " Y座標：" + mY;
-  stalker.style.transform = `translate(${mX}px, ${mY}px)`
-  
-}
+var mX = 0; // マウスカーソルの座標
+var mY = 0; // マウスカーソルの座標
+var nX = 0; // ストーカーの座標
+var nY = 0; // ストーカーの座標
 
+function getMouseXY(e) {
+  mX = e.pageX;
+  mY = e.pageY;
+}
 document.onmousemove = getMouseXY;
+
+setInterval(function(){
+  if(mX > nX){
+    nX += 3;
+  } else {
+    nX -= 3;
+  }
+  if(mY > nY){
+    nY += 3;
+  } else {
+    nY -= 3;
+  }
+  stalker.style.left = nX + 'px';
+  stalker.style.top = nY + 'px';
+}, 200);
